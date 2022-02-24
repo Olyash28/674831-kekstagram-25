768c6a4cb989ddbd829f7b6166cc18dd69753839
Olk <Olyash28@gmail.com>
2022-02-24 14:33:44 +0300

    доработки

--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
-function getRandomNumber (firstNumber, lastNumber) {
-  if(firstNumber >= 0 && lastNumber > firstNumber) {
-    return Math.round(Math.random() * (lastNumber - firstNumber + 1)) + firstNumber;
-  } else {
-    alert('Напишите что-нибудь другое');
+function getRandomNumber(firstNumber, lastNumber) {
+  if (firstNumber >= 0 && lastNumber > firstNumber) {
+    return Math.floor(Math.random() * (lastNumber - firstNumber + 1)) + firstNumber;
   }
+  return ('Напишите что-нибудь другое');
 }
-getRandomNumber(5,8);
 
+getRandomNumber(5, 8);
+
+//Ссылки на источники:
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-// https://learn.javascript.ru/alert-prompt-confirm
+// https://learn.javascript.ru/number
 
 
-let message;
-const maxLength = 140;
-let findLength = 0;
-function checkComment (message, findLength) {
-  findLength = message.length;
-  if (findLength <= maxLength) {
-    return true;
-  }
-    return false;
+function checkCommentLength(message, maxLength) {
+  const messageLength = message.length;
+
+  return (messageLength <= maxLength);
 }
-checkComment('приветик');
+
+checkCommentLength('приветик', 140);
