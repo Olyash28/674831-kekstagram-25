1b02e95b72bad23299c1d4668b061ae36d370cb9
Olk <Olyash28@gmail.com>
2022-03-10 11:48:10 +0300

    Модуляция

---
+++ js/util.js
@@ -0,0 +1,19 @@
+function getRandomNumber(firstNumber, lastNumber) {
+  if (firstNumber >= 0 && lastNumber > firstNumber) {
+    return Math.floor(Math.random() * (lastNumber - firstNumber + 1)) + firstNumber;
+  }
+  return ('Напишите что-нибудь другое');
+}
+
+export {getRandomNumber};
+//Ссылки на источники:
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// https://learn.javascript.ru/number
+
+
+function checkCommentLength(message, maxLength) {
+  const messageLength = message.length;
+
+  return (messageLength <= maxLength);
+}
+checkCommentLength();
